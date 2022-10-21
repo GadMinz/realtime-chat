c361a21c211b10baf41d5804450fbfda54c12f4b
GadMinz <baykov97@mail.ru>
2022-10-21 16:24:17 +0300

    Add info in navbar

--- src/components/SideBar/NavBar.tsx
+++ src/components/SideBar/NavBar.tsx
@@ -3,18 +3,23 @@ import s from "./SideBar.module.scss";
 import logoutImg from "../../assets/icons/logout.svg";
 import { auth } from "../../firebase";
 import { signOut } from "firebase/auth";
+import { useAppSelector } from "../../hook";
+import avatar from "../../assets/img/avatar.jpg";
 
 interface NavBarProps {}
 
 const NavBar: React.FC<NavBarProps> = () => {
+  const { currentUser } = useAppSelector((state) => state.auth);
+  if (!currentUser) return null;
+
   return (
     <div className={s.navbar}>
       <div className={s.user}>
         <img
-          src="https://pixelbox.ru/wp-content/uploads/2021/05/ava-vk-animal-91.jpg"
+          src={currentUser.photoURL ? currentUser.photoURL : avatar}
           alt="avatar"
         />
-        <span>GadMinz</span>
+        <span>{currentUser.nickname}</span>
       </div>
       <button onClick={() => signOut(auth)}>
         <img src={logoutImg} alt="logout" />
